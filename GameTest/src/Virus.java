11ac0189bab21f06653a62418c2e416b5bd83a65
MountAndDewMe <25701594+MountAndDewMe@users.noreply.github.com>
2017-11-12 08:56:04 -1000

    Tweaked devin's code
    Since our rad is random values now, we do not want it static.
    Changed how mass is initialized (mass = rad * 2)
    Changed some public variables into private
    Removed unnecessary methods
    Created a more pleasing circle with an outline

--- GameTest/src/Virus.java
+++ GameTest/src/Virus.java
@@ -2,58 +2,23 @@ import java.awt.Color;
 import java.util.Random;
 
 public class Virus {
+	public static final int MAX_VIRUS = 10;
 	public int x;
 	public int y;
-	public static int rad;
-	private int r, g, b;
-	private Color c;
-	public EZCircle virus;
-	public static final int MAX_VIRUS = 10;
-	public static double mass = 50; 
+	public int rad;
+	public int mass;
+	
+	private EZCircle virus;
 	
 	public Virus(int x, int y, int rad) {
 		this.x = x;
 		this.y = y;
 		this.rad = rad;
+		this.mass = rad * 2;
 		
-		if(isRGBNotWhite()) {
-			drawVirus();
-		}
+		virusOutline = EZ.addCircle(x, y, rad * 2, rad * 2, Color.yellow, true);
+		virus = EZ.addCircle(x, y, rad * 2, rad * 2, Color.green, true);
 		
-	}
-	public void drawVirus() {
-		virus = EZ.addCircle(x, y, rad*2, rad*2, Color.black, true);
 	}
 	
-		
-	private boolean isRGBNotWhite() {
-		Random rand = new Random();
-
-		r = rand.nextInt(255);
-		g = rand.nextInt(255);
-		b = rand.nextInt(255);
-		
-		while(r > 220 && g > 220 && b > 220) {
-			r = rand.nextInt(255);
-			g = rand.nextInt(255);
-			b = rand.nextInt(255);
-		}
-		
-		this.c = new Color(r, g, b);
-		return true;
-	}
-	
-	public int getVirusXCenter() {
-		return virus.getXCenter();
-	}
-	
-	public int getVirusYCenter() {
-		return virus.getYCenter();
-	}
-	
-	public boolean isVirusPointInElement(int x, int y) {
-		return virus.isPointInElement(x, y);
-	}
-	
-
 }
