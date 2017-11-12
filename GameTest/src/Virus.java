a2fd6ded5c535a0b55cba06fde83bd76dd48259d
MountAndDewMe <25701594+MountAndDewMe@users.noreply.github.com>
2017-11-12 09:17:18 -1000

    Fixed virusOutline to display properly
    Added a border

--- GameTest/src/Virus.java
+++ GameTest/src/Virus.java
@@ -3,6 +3,7 @@ import java.util.Random;
 
 public class Virus {
 	public static final int MAX_VIRUS = 10;
+	public static final int BORDER = 8;
 	public int x;
 	public int y;
 	public int rad;
@@ -16,7 +17,7 @@ public class Virus {
 		this.rad = rad;
 		this.mass = rad * 2;
 		
-		virusOutline = EZ.addCircle(x, y, rad * 2, rad * 2, Color.yellow, true);
+		virusOutline = EZ.addCircle(x, y, rad * 2 + BORDER, rad * 2 + BORDER, Color.yellow, true);
 		virus = EZ.addCircle(x, y, rad * 2, rad * 2, Color.green, true);
 		
 	}
